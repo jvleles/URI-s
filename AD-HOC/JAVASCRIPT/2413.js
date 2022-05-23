ccc7736424ee336ccdcc91dade005918089a5c49
João Victor <105935324+jvleles@users.noreply.github.com>
2022-05-23 20:02:37 -0300

    2413 JavaScript Solution

---
+++ AD-HOC/JAVASCRIPT/2413.js
@@ -0,0 +1,8 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+var prompt = function(texto) { return lines.shift(); };
+
+var N = parseInt(prompt(""))
+var conta = N*4
+
+console.log(conta)

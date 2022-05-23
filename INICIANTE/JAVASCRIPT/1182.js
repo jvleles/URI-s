9763a2ba6688b252e5e92a01c354feb1a07bbb54
João Victor <105935324+jvleles@users.noreply.github.com>
2022-05-23 19:20:31 -0300

    1182 JavaScript Solution

---
+++ INICIANTE/JAVASCRIPT/1182.js
@@ -0,0 +1,27 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+var prompt = function(texto) { return lines.shift(); };
+
+var coluna = parseInt(prompt(""))
+var operacao = prompt("")
+var x = []
+var soma = 0
+
+for(i = 0; i < 12; i++){
+    x[i] = []
+    for(j = 0; j < 12; j++){
+        x[i][j] = parseFloat(prompt(""))
+    }
+}
+
+for(k = 0; k < 12; k++){
+    soma = soma + x[k][coluna]
+}
+
+if(operacao == "S"){
+    console.log(soma.toFixed(1))
+}
+else if(operacao == "M"){
+    var media = soma/12
+    console.log(media.toFixed(1))
+}

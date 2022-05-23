a779085584d20e9967eb9b3cc90b402f91b9c7d3
João Victor <105935324+jvleles@users.noreply.github.com>
2022-05-23 19:03:21 -0300

    1096 JavaScript Solution

---
+++ INICIANTE/JAVASCRIPT/1096.js
@@ -0,0 +1,14 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var I = 1
+var J = 7
+
+for(i=0;i<5;i++){
+    for(k = 0;k<3;k++){
+        console.log("I=" + I + " J=" + J)
+        J = J - 1
+    }
+    var J = 7
+    I = I + 2
+}

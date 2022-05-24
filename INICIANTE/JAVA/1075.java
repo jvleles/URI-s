60cac6cae1f49fc01bea4defa9f4a9e00751a938
João Victor <105935324+jvleles@users.noreply.github.com>
2022-05-23 22:06:38 -0300

    1075 Java Solution

---
+++ INICIANTE/JAVA/1075.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for(int x = 0; x <= 10000; x++)	{
+            if(x % N == 2){
+                System.out.println(x);
+            }
+        }
+        sc.close();
+    }
+}

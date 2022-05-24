a2f2f53bf1fe60557ac2b9b0afae0bbc2f0b99b3
João Victor <105935324+jvleles@users.noreply.github.com>
2022-05-23 21:51:29 -0300

    1016 Java Solution

---
+++ INICIANTE/JAVA/1016.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int x, y;
+        Scanner sc = new Scanner(System.in);
+        x = sc.nextInt();
+        y = 2*x;
+        System.out.println(y + " minutos");
+    }
+}

f04b8127dfec1355821361c6201524020c6ece23
João Victor <105935324+jvleles@users.noreply.github.com>
2022-05-23 21:53:34 -0300

    1041 Java Solution

---
+++ INICIANTE/JAVA/1041.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double x, y;
+        Scanner sc = new Scanner(System.in);
+        x = sc.nextDouble();
+        y = sc.nextDouble();
+        
+        if(x > 0 && y > 0){
+            System.out.println("Q1");
+        }
+        else if(x > 0 && y < 0){
+            System.out.println("Q4");
+        }
+        else if(x < 0 && y > 0){
+            System.out.println("Q2");
+        }
+        else if(x < 0 && y < 0){
+            System.out.println("Q3");
+        }
+        else if(x == 0 && y != 0){
+            System.out.println("Eixo Y");
+        }
+        else if(y == 0 && x != 0){
+            System.out.println("Eixo X");
+        }
+        else if(x == 0 && y == 0){
+            System.out.println("Origem");
+        }
+        sc.close();
+    }
+}

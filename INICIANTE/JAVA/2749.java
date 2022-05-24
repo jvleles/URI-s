f02fc15300e3a69d4e21e4f8919737231d12a147
João Victor <105935324+jvleles@users.noreply.github.com>
2022-05-24 11:24:14 -0300

    2749 Java Solution

---
+++ INICIANTE/JAVA/2749.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        System.out.println("---------------------------------------");
+        System.out.println("|x = 35                               |");
+        System.out.println("|                                     |");
+        System.out.println("|               x = 35                |");
+        System.out.println("|                                     |");
+        System.out.println("|                               x = 35|");
+        System.out.println("---------------------------------------");
+
+    }
+ 
+}
